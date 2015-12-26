a3f7cc2166738f2dc83fa2d9ccf54b002f41ddc1
Robert <robertandrewromano@gmail.com>
2015-12-26 00:53:06 -0500

    Add turn function to snake prototype.

--- js/snake.js
+++ js/snake.js
@@ -2,3 +2,17 @@ var Snake = function () {
   this.dir = "N";
   this.segments = [0,0];
 }
+
+Snake.prototype.turn = function (dir) {
+  if (this.dir == "N" && dir == "S") {
+    return;
+  } else if (this.dir == "W" && dir == "E") {
+    return;
+  } else if (this.dir == "E" && dir == "W") {
+    return;
+  } else if (this.dir == "S" && dir == "N") {
+    return;
+  } else {
+    this.dir = dir;
+  }
+};
